1379d1c563d25f55c2c638e41ae7c4f8292edb70
Ravana <47528708+ravana69@users.noreply.github.com>
2021-09-21 21:37:59 +0530

    Update playback.js

--- playback.js
+++ playback.js
@@ -19,7 +19,7 @@ const sources = [
 ];
 
 const labels = [
-  [ "Ravana Fm", "http://stream.zeno.fm/y7n8qfmh838uv"],
+  [ "Ravana FM", "http://stream.zeno.fm/y7n8qfmh838uv"],
   [ "Ravana FM 2", "http://stream.zeno.fm/39zagfub938uv"],
   [ "94.3", "https://playerservices.streamtheworld.com/api/livestream-redirect/CLUBFMUAEAAC.aac"],
   [ "Gramaphone FM", "https://edge.mixlr.com/channel/axxln"],
