b392cb38f9fa2e5cc36f73e66d9522c833c852d4
Ravana <47528708+ravana69@users.noreply.github.com>
2021-09-22 12:49:12 +0530

    Update playback.js

--- playback.js
+++ playback.js
@@ -5,7 +5,6 @@ const sources = [
   "https://edge.mixlr.com/channel/axxln", // Gramaphone FM
   "https://listen.radioking.com/radio/305023/stream/354512", // Patupetty FM
   "https://prclive4.listenon.in/Malayalam", // Radio City Malayalam
-  "https://schedule.radiolemonlive.com:7443/radio/8000/live", // Radio Lemon Live
   "https://servidor15.brlogic.com:7110/live", // Radio Green
   "https://streamer.radio.co/sd0b826808/listen", // Radio Beats
   "http://radio.knpinfotech.co.in:8000/stream/1/", // KNP Infotech
@@ -25,7 +24,6 @@ const labels = [
   [ "Gramaphone FM", "https://edge.mixlr.com/channel/axxln"],
   [ "Patupetty FM", "https://listen.radioking.com/radio/305023/stream/354512"],
   [ "Radio City Malayalam", "https://prclive4.listenon.in/Malayalam"],
-  [ "Radio Lemon Live", "https://schedule.radiolemonlive.com:7443/radio/8000/live"],
   [ "Radio Green", "https://servidor15.brlogic.com:7110/ve"],
   [ "Radio Beats", "https://streamer.radio.co/sd0b826808/listen"],
   [ "KNP Infotech", "http://radio.knpinfotech.co.in:8000/stream/1/?listening-from-radio-garden=1624554409280"],
